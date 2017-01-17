2e70917561b662a40e03a641fbefa2ec410c9fbe
caseypc <caseypc@users.noreply.github.com>
2017-01-17 14:08:03 -0600

    phone number formatting

--- PHP/functions.php
+++ PHP/functions.php
@@ -981,4 +981,49 @@ private function ReverseIPOctets($inputip){
     $ipoc = explode(".",$inputip);
     return $ipoc[3].".".$ipoc[2].".".$ipoc[1].".".$ipoc[0];
 }
+/*--------------------------------------------------------------------------------
+| Format Phone Numbers - returns areacode, exchange, and number
+|--------------------------------------------------------------------------------*/
+function formatPhone($number) {
+	$onlynums = preg_replace('/[^0-9]/','',$number);
+	$areacode = substr($onlynums, 0,3);
+	$exchange = substr($onlynums,3,3);
+	$number = substr($onlynums,6,4);
+	return array(
+		'areacode' => $areacode,
+		'exchange' => $exchange,
+		'number' => $number
+	);
+}
+/*--------------------------------------------------------------------------------
+| Format Phone Numbers - with extensions and with a little bit of work country codes
+|--------------------------------------------------------------------------------*/
+function sanitize_phone($phone, $international = false) {
+	$format = "/(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/";
+	$alt_format = '/^(\+\s*)?((0{0,2}1{1,3}[^\d]+)?\(?\s*([2-9][0-9]{2})\s*[^\d]?\s*([2-9][0-9]{2})\s*[^\d]?\s*([\d]{4})){1}(\s*([[:alpha:]#][^\d]*\d.*))?$/';
+	// Trim & Clean extension
+	$phone = trim($phone);
+	$phone = preg_replace( '/\s+(#|x|ext(ension)?)\.?:?\s*(\d+)/', ' ext \3', $phone );
+	if ( preg_match( $alt_format, $phone, $matches ) ) {
+		// return '('.$matches[4].') '.$matches[5].'-'.$matches[6].(!empty($matches[8]) ? ' '.$matches[8] : '' );
+		return array(
+			'areacode' => $matches[4],
+			'exchange' => $matches[5],
+			'number' => $matches[6],
+			'extension' => (!empty($matches[8]) ? $matches[8] : '')
+		);
+	} elseif (preg_match($format, $phone, $matches)) {
+		// format
+		$phone = preg_replace($format, "($2) $3-$4", $phone);
+		// Remove likely has a preceding dash
+		$phone = ltrim( $phone, '-' );
+		// Remove empty area codes
+		if (false !== strpos(trim($phone), '()', 0)) { 
+			$phone = ltrim(trim($phone), '()');
+		}
+		// Trim and remove double spaces created
+		return preg_replace('/\\s+/', ' ', trim($phone));
+	}
+	return false;
+}
 ?>
